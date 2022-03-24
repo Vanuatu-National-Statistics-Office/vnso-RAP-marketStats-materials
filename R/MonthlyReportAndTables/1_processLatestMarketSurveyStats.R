0101fb823b715b235f1433e2009fb3c79b94060c
hugo-e-pigott <hpigott@vanuatu.gov.vu>
2022-03-24 15:57:46 +1100

    cleaning up repository

--- R/MonthlyReportAndTables/1_processLatestMarketSurveyStats.R
+++ R/MonthlyReportAndTables/1_processLatestMarketSurveyStats.R
@@ -1,3 +1,126 @@
+#### Preparation ####
+
+# Clear the environment
+rm(list = ls())
+
+# Load the required libraries
+library(dplyr) # Manipulating data
+library(stringr) # common string operations
+
+# Note where VNSO code/data is on current computer
+repository <- file.path(dirname(rstudioapi::getSourceEditorContext()$path), "..", "..")
+setwd(repository) # Required for file.choose() function
+
+# Load the general R functions
+source(file.path(repository, "R", "functions.R"))
+
+# Note the secure data path
+secureDataFolder <- file.path(repository, "data", "secure")
+
+# Note the open data path
+openDataFolder <- file.path(repository, "data", "open")
+
+# Read in the raw trade data from secure folder of the repository 
+tradeStatsFile <- file.path(secureDataFolder, "SEC_PROC_ASY_RawDataAndReferenceTables_31-01-20.csv")
+tradeStats <- read.csv(tradeStatsFile, header=TRUE, na.strings=c("","NA", "NULL", "null")) #replace blank cells with missing values-NA
+
+
+
+#Processing of the Market Survey MS1 Collection
+
+#Load library
+library(dplyr)
+
+
+
+
+#Load MS1 Files Data Processing
+
+ms1_master <- read.delim("data/secure/ms1/VNSOMS2019.tab")
+staple_roster <- ms1 <- read.delim("data/secure/ms1/staple_roster.tab")
+vegetable_roster <- ms1 <- read.delim("data/secure/ms1/Vegetable_roster.tab")
+fruit_roster <- ms1 <- read.delim("data/secure/ms1/fruits_roster.tab")
+
+#Remove unwanted columns
+
+ms1_master_extract <- ms1_master[ , c(1,2,3,4,5,6,17,28)]
+ms1_master_extract$id <- ms1_master_extract$ï..interview__key
+staple_roster$id <- staple_roster$ï..interview__key
+ms1_staple <- merge(ms1_master_extract, staple_roster, by = "id")
+
+
+
+
+
+
+#Merge Master with Roster
+
+
+
+#Processing of Market Survey MS2 Collection
+
+#Load MS2 Files Data Processing
+
+ms2_master <- read.delim("data/secure/ms2/VNSOMCS.tab")
+ms2_staple_roster <- read.delim("data/secure/ms2/root_crop_roster.tab")
+ms2_vegetable_roster <- read.delim("data/secure/ms2/vegis_roster.tab")
+ms2_fruit_roster <- read.delim("data/secure/ms2/fruits_roster.tab")
+
+
+#Remove unwanted columns
+
+ms2_master_extract <- ms2_master[ , c(1,2,3,9,10,11,12)]
+ms2_master_extract$id <- ms2_master_extract$ï..interview__key
+ms2_staple_roster$id <- ms2_staple_roster$ï..interview__key
+ms2_staple <- merge(ms2_master_extract, ms2_staple_roster, by = "id")
+
+
+# Calculating the Average for stable
+ms2_staple$averageprice <- (ms2_staple$staple_price1 + ms2_staple$staple_price2 + ms2_staple$staple_price3 + ms2_staple$staple_price4 + ms2_staple$staple_price5)/5
+ms2_staple$averagewieght <- (ms2_staple$staple_wieght1 + ms2_staple$staple_wieght2 + ms2_staple$staple_wieght3 + ms2_staple$staple_wieght4 + ms2_staple$staple_wieght5)/5
+
+#Remove unwanted columns for Fruits
+ms2_master_extract <- ms2_master[ , c(1,2,3,9,10,11,12)]
+ms2_master_extract$id <- ms2_master_extract$ï..interview__key
+ms2_fruit_roster$id <- ms2_fruit_roster$ï..interview__key
+ms2_fruit <- merge(ms2_master_extract, ms2_fruit_roster, by = "id")
+
+# Calculating the Average for fruit
+ms2_fruit$averageprice <- (ms2_fruit$fruit_price1 + ms2_fruit$fruit_price2 + ms2_fruit$fruit_price3 + ms2_fruit$fruit_price4 + ms2_fruit$fruit_price5)/5
+ms2_fruit$averagewieght <- (ms2_fruit$fruit_weight1 + ms2_fruit$fruit_weight2 + ms2_fruit$fruit_weight3 + ms2_fruit$fruit_weight4 + ms2_fruit$fruit_weight5)/5
+
+#Removing unwanted columns for Vegetables
+ms2_master_extract <- ms2_master[ , c(1,2,3,9,10,11,12)]
+ms2_master_extract$id <- ms2_master_extract$ï..interview__key
+ms2_vegetable_roster$id <- ms2_vegetable_roster$ï..interview__key
+ms2_vegetable <- merge(ms2_master_extract, ms2_vegetable_roster, by = "id")
+
+# Calculating the Average for Vegetables
+ms2_vegetable$averageprice <- (ms2_vegetable$vegetable_price1 + ms2_vegetable$vegetable_price2 + ms2_vegetable$vegetable_price3 + ms2_vegetable$vegetable_price4 + ms2_vegetable$vegetable_price5)/5
+ms2_vegetable$averagewieght <- (ms2_vegetable$vegetables_weight1 + ms2_vegetable$vegetables_weight2 + ms2_vegetable$vegetables_weight3 + ms2_vegetable$vegetables_weight4 + ms2_vegetable$vegetables_weight5)/5
+
+
+
+#Processing of the Market Survey MS2 Collection
+
+#Load library
+library(dplyr) #Data manipulation
+library(readxl) #read in Excel files
+library(tibble)
+library(tidyverse)
+library(RSQLite) #R SQLite driver package
+library(DBI) #Database driver package
+
+#Mapping of the directory path
+setwd(paste0(getwd()))
+getwd()
+
+#Establish connection the the SQLite database
+mydb <- dbConnect(RSQLite::SQLite(), "data/secure/ms2/sqlite/ms2.sqlite")
+
+
+#### Processing of Market Survey MS1 Collection ####
+
 #Processing of the Market Survey MS2 Collection
 
 #Load library
@@ -117,7 +240,6 @@ ms2_staple_collection <- dbGetQuery(mydb, "SELECT ms2_master.id,
                                                   ms2_staple_measurement.staple_price4,
                                                   ms2_staple_measurement.staple_wieght5,
                                                   ms2_staple_measurement.staple_price5
-
                                             FROM ms2_master
                                             
                                             INNER JOIN ms2_market ON ms2_master.market = ms2_market.market_id
@@ -216,7 +338,8 @@ Fiji_Taro_Weights <- ms2_staple_collection %>%
   dplyr::mutate(avg_price = ((staple_price1+staple_price2+staple_price3+staple_price4+staple_price5)/5))
 
 
-  
+
 
 
 dbDisconnect(mydb)
+
