c7ec62bacb75886e632fd8c793a32565bd9ad56f
Jeroen Minderman <Jeroen.Minderman@ons.gov.uk>
2022-05-25 15:47:55 +0100

    Added write Excel examples

--- R/excel_examples.r
+++ R/excel_examples.r
@@ -103,4 +103,87 @@ df1 <- read.xlsx(finalWorkbookFileName, #filepath to import
                  rows = c(3:25), # which rows to import. rows = NULL to import all
                  skipEmptyRows = TRUE # don't bring in empty rows
                  )
-  
+
+
+
+# writing excel sheets
+
+# Using 'openxlsx' library for XLSX writing functionality
+install.packages("openxlsx")
+library(openxlsx)
+
+# Here are a couple of examples setting out the use of openxlsx.
+# The package has a lot of functionality to control exact formatting (colours, fonts, etc.)
+# https://cran.r-project.org/web/packages/openxlsx/vignettes/Introduction.html
+# https://cran.r-project.org/web/packages/openxlsx/vignettes/Formatting.html
+
+# Just for illustration purposes, we use ms2_staple_collection.csv:
+staples <- read.csv("data/open/ms2/ms2_staple_collection.csv")
+
+# To write to an Excel file and use separate sheets, we first set up a
+# new Excel workbook object to write to:
+
+wb <- createWorkbook()
+# Adding a new sheet to this workbook:
+addWorksheet(wb, sheetName = "Staples1")
+
+### Simple writing of a new spreadsheet
+
+# writeDataTable() writes data x to a given sheet. 
+# Note setting tableStyle to "none" and withFilter to FALSE ensures fully plain formatting.
+# More details on this (and options) are in the help file for the function:
+# https://www.rdocumentation.org/packages/openxlsx/versions/4.2.5/topics/writeDataTable
+writeDataTable(wb, sheet = "Staples1", 
+                   x = staples, 
+                   tableStyle = "none",
+                   withFilter = FALSE)
+
+# To save the file, we then use saveWorkbook()
+saveWorkbook(wb, "outputs/openxlsx_demo1.xlsx", overwrite = TRUE) 
+
+
+### Adding a second sheet and writing to a specific position:
+
+# Another example, adding a further sheet:
+addWorksheet(wb, sheetName = "MoreStaples")
+
+# Write the same table into this sheet but now starting at a given row and column:
+
+# First set some better column names so we don't have to deal with that in Excel:
+colnames(staples) <- c("Type", "Week 1", "Week 2") 
+# Write data to new sheet:
+writeDataTable(wb, sheet = "MoreStaples", 
+                   x = staples, 
+                   startCol = 2, 
+                   startRow = 3,
+                   tableStyle = "none",
+                   withFilter = FALSE)
+
+# Save the workbook again:
+# (best to close Excel and reopen while you're doing this)
+saveWorkbook(wb, "outputs/openxlsx_demo1.xlsx", overwrite = TRUE) 
+
+
+### Editing data in single cells & merging cells:
+
+# Replace the value in col 3, row 2 of sheet MoreStaples with "January":
+writeData(wb, sheet = "MoreStaples", 
+                   x = "January", 
+                   startCol = 3, 
+                   startRow = 2)
+saveWorkbook(wb, "outputs/openxlsx_demo1.xlsx", overwrite = TRUE) 
+
+# merging cells - so that "January" spans both columns:
+mergeCells(wb, sheet = "MoreStaples",
+               cols = 3:4, rows = 2)
+saveWorkbook(wb, "outputs/openxlsx_demo1.xlsx", overwrite = TRUE) 
+
+# Replacing a range of values.
+# Note that when given a vector (1,2,3,4) this writes by column (vertically) from a given starting position.
+# Also note we use xy instead of startCol and startRow here:
+writeData(wb, sheet = "MoreStaples", 
+                   x = c(1,2,3,4), 
+                   xy = c(4,4))
+saveWorkbook(wb, "outputs/openxlsx_demo1.xlsx", overwrite = TRUE) 
+
+
